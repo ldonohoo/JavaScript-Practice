57ab360d7e87e53bb1364863ac84d8337f9784c6
Lisa Donohoo <38710969+ldonohoo@users.noreply.github.com>
2023-12-20 20:41:19 -0600

    Create possibleUsernames.js

---
+++ scripts/possibleUsernames.js
@@ -0,0 +1,10 @@
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z][a-z]+\d*$|^[a-z]\d+\d+$/ig; // Change this line
+let result = userCheck.test(username);
+
+/*
+Usernames can only use alphanumeric characters.
+The only numbers in the username have to be at the end. There can be zero or more of them at the end. Username cannot start with the number.
+Username letters can be lowercase and uppercase.
+Usernames have to be at least two characters long. A two-character username can only use alphabet letters as characters.
+*/
